793568e2b65b2c6ada042cec8807c7db74a863ad
427Noam <427noam@gmail.com>
2023-06-10 22:31:52 +0300

    update

--- src/main/java/com/example/server_ip/ServerIpApplication.java
+++ src/main/java/com/example/server_ip/ServerIpApplication.java
@@ -14,28 +14,6 @@ public class ServerIpApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ServerIpApplication.class, args);
-        /*HttpServer server = null;
-        try {
-            server = HttpServer.create(new InetSocketAddress(8000), 0);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        server.createContext("/get_time", new TimeHandler());
-        server.setExecutor(null);
-        server.start();
-        System.out.println("Server is running on port 8000");
-    }
-
-    static class TimeHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            String response = java.time.LocalDateTime.now().toString();
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }*/
 
     }
 }
